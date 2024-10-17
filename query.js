8511adb324fa65077c0438bfa9045517f358f3dc
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-10-16 19:53:27 -0500

    problem five user prompt 2 complete

--- query.js
+++ query.js
@@ -3,23 +3,28 @@ const db = require('./db')
 const { Flight } = require('./models')
 const { Airport } = require('./models')
 
-const findAll = async () => {
-  let flightsArray = await Flight.find({})
-  let airportsArray = await Airport.find({})
-  let getInfo = flightsArray.map(flight => {
-    airportsArray.forEach(airport => {
-      if (flight.departingAirport === airport.id) {
-        flight._doc.airport = airport.name
-      }
-    })
-    return flight
-  })
-  console.log(getInfo)
+// const findAll = async () => {
+//   let flightsArray = await Flight.find({})
+//   let airportsArray = await Airport.find({})
+//   let getInfo = flightsArray.map(flight => {
+//     airportsArray.forEach(airport => {
+//       if (flight.departingAirport === airport.id) {
+//         flight._doc.airport = airport.name
+//       }
+//     })
+//     return flight
+//   })
+//   console.log(getInfo)
+// }
+
+const findFlightById = async(id) => {
+  const flight = await Flight.findById()
+  console.log(flight)
 }
 
 async function main() {
   try {
-    await findAll()
+    await findFlightById()
   } catch (error) {
     console.log(error)
   } finally {
