ded996dd1781add3ac192ab7f704a42ab161677e
rhiannon-obrien-coder <rhiannonobrien@gmail.com>
2024-10-17 21:47:10 -0500

    fixed queries for real this time tho

--- query.js
+++ query.js
@@ -3,74 +3,112 @@ const db = require('./db')
 const { Flight } = require('./models')
 const { Airport } = require('./models')
 
-// const findAll = async () => {
-//   let flightsArray = await Flight.find({})
-//   let airportsArray = await Airport.find({})
-//   let getInfo = flightsArray.map(flight => {
-//     airportsArray.forEach(airport => {
-//       if (flight.departingAirport === airport.id) {
-//         flight._doc.airport = airport.name
-//       }
-//     })
-//     return flight
-//   })
-//   console.log(getInfo)
-// }
+const findAll = async () => {
+  let flightsArray = await Flight.find({})
+  let airportsArray = await Airport.find({})
+  flightsArray.forEach(flight => {
+    let depoAirpo = ""
+    let arivAirpo = ""
+    airportsArray.forEach(airport => {
+      if (flight.departingAirport.toString() === airport.id) {
+        depoAirpo = airport.name
+        console.log("match!")
+      }
+      if (flight.arrivalAirport.toString() === airport.id) {
+        arivAirpo = airport.name
+        console.log("match!")
+      }
+    })
+    console.log(`${flight.flightNumber}, ${flight.airline}, Departing Airport: ${depoAirpo}, Arriving Airport: ${arivAirpo}, ${flight.departureDateTime}`)
+  })
+}
 
-// const findFlightById = async(id) => {
-//   const flight = await Flight.findById()
-//   console.log(flight)
-// }
+const findFlightById = async(id) => {
+  const flight = await Flight.findById(id)
+  console.log(flight)
+}
 
-// const findAirportById = async(id) => {
-//   const airport = await Airport.findById()
-//   console.log(airport)
-// }
+const findAirportById = async(id) => {
+  const airport = await Airport.findById(id)
+  console.log(airport)
+}
 
 const createFlight = async () => {
+  const kcmo = await Airport.find({ code: 'MCI' })
+  const austx = await Airport.find({ code: 'AUS' })
+  const orlando = await Airport.find({ code: 'MCO' })
   const flight = await Flight.create({
-    airline: "Southwest Airlines",
-    flightNumber: 'WS1234',
-    price: 243,
-    numberOfSeats: 39,
-    departingAirport: orlando[0]._id, 
-    arrivalAirport: austx[0]._id, 
-    departureDateTime: "2024-10-22T22:00:00",
+    airline: "TestLine",
+    flightNumber: 'Test',
+    price: 53,
+    numberOfSeats: 12,
+    departingAirport: austx[0]._id, 
+    arrivalAirport: orlando[0]._id, 
+    departureDateTime: "2024-12-22T23:00:00",
   })
   console.log(flight)
 }
 
 const createAirport = async () => {
   const airport = await Airport.create({
-    name: "Kansas City International Airport",
-    location: "Kansas City, Missouri",
-    code: "MCI"
+    name: "Test",
+    location: "Text City",
+    code: "TST"
   })
   console.log(airport)
 }
 
-// const deleteFlight = async () => {
-//   const flight = await Flight.deleteOne({ flightNumber: 'UA1234' })
-//   console.log(flight)
-// }
+const deleteFlight = async (id) => {
+  const flight = await Flight.findByIdAndDelete(id)
+  console.log(flight)
+}
 
-// const deleteAirport = async () => {
-//   const airport = await Airport.deleteOne({ code: 'AUS' })
-//   console.log(airport)
-//}
+const deleteAirport = async (id) => {
+  const airport = await Airport.findByIdAndDelete(id)
+  console.log(airport)
+}
 
-const updateFlight = async () => {
-  const updated = await Flight.updateOne(
-      { departureDateTime: "2024-10-22T19:00:00" },
-      { departureDateTime: "2024-10-22T19:30:00" }
-  )
+const updateFlightById = async(id, newFlight) => {
+  const updated = await Flight.findByIdAndUpdate(id, newFlight, { new : true })
+  console.log(updated)
+}
+
+const updateAirportById = async(id, newAirport) => {
+  const updated = await Airport.findByIdAndUpdate(id, newAirport, { new : true })
   console.log(updated)
 }
 
 async function main() {
+  const kcmo = await Airport.find({ code: 'MCI' })
+  const austx = await Airport.find({ code: 'AUS' })
+  const orlando = await Airport.find({ code: 'MCO' })
   try {
-    await createFlight()
-    await createAirport()
+    // await findAll()
+    // await findFlightById("6711c187c7ef595a811b4c38")
+    // await findAirportById("6711c17deed26b26b1634a4c")
+    // await createAirport()
+    // await createFlight()
+
+    // await updateFlightById("6711c68b6c3f12e980928543",
+    //   {
+    //     airline: "Northeast Airlines",
+    //     flightNumber: 'WS1664',
+    //     price: 456,
+    //     numberOfSeats: 78,
+    //     departingAirport: orlando[0]._id, 
+    //     arrivalAirport: kcmo[0]._id, 
+    //     departureDateTime: "2024-10-22T22:00:00",
+    //   }
+    // )
+
+    // await updateAirportById("6711c17deed26b26b1634a4c", {
+    //   name: "Kansas City Airport",
+    //   location: "Kansas City",
+    //   code: "MCI"
+    // })
+
+    await deleteFlight("6711c68b6c3f12e980928543")
+    await deleteAirport("6711c4fbebbea501c7ef0e88")
   } catch (error) {
     console.log(error)
   } finally {
